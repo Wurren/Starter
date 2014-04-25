ed0fc1807c0506b738ec2b40ecc0825d4ddb9bdd
Warren Haskins <warrenhaskins@gmail.com>
2014-04-25 16:44:10 +0100

    Fixes JS reload on watch

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('watch', function() {
      gulp.watch('less/*.less', ['less']);
      gulp.watch('js/src/*.js', ['uglify']);
      var server = livereload();
-     gulp.watch(['css/*.css', 'js/src/*', '*.html']).on('change', function(file) {
+     gulp.watch(['css/*.css', 'js/*.js', '*.html']).on('change', function(file) {
           server.changed('*.html');
      });
 });
